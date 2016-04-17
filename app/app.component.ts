4c36136f9c81085437de686f83198b9d15b77df8
Harish93 <harishbalajiit@gmail.com>
2016-04-17 11:16:18 -0700

    updated

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from 'angular2/core';
 import {LoginFormComponent} from './login-form.component'
+import {SignupFormComponent} from './signup-form.component'
 
 @Component({
     selector: 'my-app',
-    directives: [LoginFormComponent],
-    template: `<login-form></login-form>`
+    directives: [LoginFormComponent,SignupFormComponent],
+    template: `<login-form></login-form><signup-form></signup-form>`
 
 })
 
---
+++ app/signup-form.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'signup-form',
+    templateUrl: 'app/signup-form.component.html'
+
+})
+
+export class SignupFormComponent { }
